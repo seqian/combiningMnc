cd85aded56b3494df3165cb2129af1d113ede652
Seqian Wang <wang030@live.com>
2014-10-01 17:43:37 -0400

    New file: combiningRunsSimple.m, which is simply a function. It expects inputBase, subjectID and sessionID, so it's still fairly rigid at the moment.

---
+++ combiningRunsSimple.m
@@ -0,0 +1,15 @@
+function combiningRunsSimple(inputFolder, subjectID, sessionID)
+
+addpath(genpath('/opt/matlab7.5/toolbox/fmristat')); addpath(genpath('/opt/matlab12b/toolbox/emma'),'-end');
+
+filename = ['subject' subjectID '_session' sessionID];
+
+input_ef = fMRIListDir([inputFolder '/data/' filename '*_mag_ef.mnc']);
+input_sd = fMRIListDir([inputFolder '/data/' filename '*_mag_sd.mnc']);
+
+X = ones(length(input_ef),1);
+contrast = 1;
+output_file_base{1} = [inputFolder '/data2/' filename '_multi'];
+mkdir([inputFolder '/data2/']);
+which_stats='_t _ef _sd';
+my_multistat(input_ef,input_sd,1,[],X,contrast,output_file_base,which_stats,[]);
